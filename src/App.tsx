0d9ad037372b55f19ca21433059694dc344849e1
Chanakarn <chanakarn.patt@ku.th>
2026-01-31 20:06:51 +0700

    change path

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Index from "./pages/Index";
 import Analysis from "./pages/Analysis";
 import History from "./pages/History";
@@ -20,6 +20,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Navigate to="/upload" replace />} />
           <Route path="/upload" element={<Index />} />
           <Route path="/analysis" element={<Analysis />} />
           <Route path="/history" element={<History />} />
